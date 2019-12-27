5e074a72aaaec462918c84151010c7fe049e0804
Luann Campos <luann-ac@outlook.com>
2019-12-27 08:57:34 -0300

    Implementado o metodo testWrap
    Que recebe uma string e um valor inteiro, e separa a string por palavras nessas quantidade de caracteres.

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -11,7 +11,21 @@ class Resolucao implements TextWrapInterface {
    * {@inheritdoc}
    */
   public function textWrap(string $text, int $length): array {
-    return [""];
+       // Case string empty, return array empty.
+       if (empty($text)) {
+        return [''];
+      }
+      else {
+        // Regex to filter break line.
+        $search = '/(.{1,' . $length . '})(?:\s|$)|(.{' . $length . '})/uS';
+        // Replacement for add tag '$' in regex.
+        $replace = '$1$2' . '$';
+        // Break by char '$' in regex.
+        $array = explode('$', preg_replace($search, $replace, $text));
+        // Remove the last empty position in the array.
+        array_pop($array);
+        return $array;
+      }
+  
   }
-
 }
